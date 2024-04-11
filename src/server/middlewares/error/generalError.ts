7a43f15fb20169e58f5278aa652186b6cb6d710d
Berta González <bertagonzalezmartin@gmail.com>
2024-04-11 10:49:05 +0200

    Add endpoint notFound middleware

---
+++ src/server/middlewares/error/generalError.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import debugCreator from "debug";
+import chalk from "chalk";
+import type { NextFunction, Request, Response } from "express";
+import CustomError from "../../CustomError/CustomError.js";
+
+export const notFound = (_req: Request, _res: Response, next: NextFunction) => {
+  const debug = debugCreator("src:server:middlewares:error");
+  debug(chalk.bgRedBright("Endpoint not found"));
+
+  const customError = new CustomError("Endpoint not found", 404);
+  next(customError);
+};
+
+export const generalError = (
+  error: CustomError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  const statusCode = error.statusCode ?? 500;
+
+  res.status(statusCode).json({ error: error.message });
+};
