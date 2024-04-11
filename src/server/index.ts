2e98deb7712662be464316c4a373fdc5ba630b29
Berta González <bertagonzalezmartin@gmail.com>
2024-04-11 10:51:22 +0200

    Add endpoint notFound path

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import pingRouter from "../features/ping/router/pingRouter.js";
 import app from "./app.js";
+import { notFound } from "./middlewares/error/generalError.js";
 
 app.use(express.json());
 
 app.use("/", pingRouter);
+
+app.use(notFound);
