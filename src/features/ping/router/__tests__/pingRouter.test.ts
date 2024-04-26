c4a13407447ef45f5c64a1f6b6d06416f4cef438
Berta González <bertagonzalezmartin@gmail.com>
2024-04-26 17:36:49 +0200

    Add pingRouter supertest

---
+++ src/features/ping/router/__tests__/pingRouter.test.ts
@@ -0,0 +1,18 @@
+import request from "supertest";
+import app from "../../../../server/app";
+import "../../../../server/index";
+
+describe("Given a GET method a '/' endpoint", () => {
+  describe("When it receives a request", () => {
+    test("Then it should respond with a message '🏓'", async () => {
+      const expectedStatusCode = 200;
+      const path = "/";
+      const expectedMessage = "🏓";
+
+      const response = await request(app).get(path).expect(expectedStatusCode);
+      const responseBody = response.body as { message: string };
+
+      expect(responseBody).toHaveProperty("message", expectedMessage);
+    });
+  });
+});
