b04f01155594ebb50c181fb1560a68014193d55a
Berta González <bertagonzalezmartin@gmail.com>
2024-05-08 11:38:19 +0200

    Fix userModel naming, sequelize instantiate, and model export

--- src/features/user/types.ts
+++ src/features/user/types.ts
@@ -1,16 +1,15 @@
 export interface UserStructure {
-  _id: string;
-  name: string;
-  lastName: string;
-  DOB: number;
-  phone: number;
+  username: string;
+  surname: string;
+  birthday: number;
+  phoneNumber: number;
   email: string;
   address: string;
   city: string;
   aboutYou: string;
-  picture: string;
-  category: string;
-  service: string;
-  serviceDescription: string;
-  password: string;
+  profileImage: string;
+  hability: string;
+  habilityTitle: string;
+  habilityDescription: string;
+  userPassword: string;
 }
